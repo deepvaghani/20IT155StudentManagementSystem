05e90aa486bbb84dad8af4d7b419be343a68d324
Deep Vaghani <20it155@charusat.edu.in>
2022-08-04 19:09:11 +0530

    Cross Origin Added

--- src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
@@ -16,16 +16,7 @@ public class StudentController {
 
     @Autowired
     StudentRepository studentRepository;
-//    List<Student> students = new ArrayList<>(
-//            Arrays.asList(
-//                    new Student(1, "Tom", "US"),
-//                    new Student(2, "Harry", "Canada"),
-//                    new Student(3, "Nick", "UK")
-//            )
-//    );
 
-    // Mappings - URL endpoints
-    // Get the list of all student
     @GetMapping("/listStudents")
     public List<Student> getAllStudents() {
         return studentRepository.findAll();

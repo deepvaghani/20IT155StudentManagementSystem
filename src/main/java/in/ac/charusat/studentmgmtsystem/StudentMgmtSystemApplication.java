c0db228531c39f53ac671f8d65d44ec1e3791b2d
Deep Vaghani <20it155@charusat.edu.in>
2022-08-08 11:47:08 +0530

    Cross Origin Added

--- src/main/java/in/ac/charusat/studentmgmtsystem/StudentMgmtSystemApplication.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/StudentMgmtSystemApplication.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "https://app-20it155studentmgmtsystem.herokuapp.com/")
 @SpringBootApplication
 public class StudentMgmtSystemApplication {
 
